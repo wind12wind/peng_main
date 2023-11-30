0941d91d72a0682fb0556310e5215b36646d93bd
youngjaele <149346316+youngjaele@users.noreply.github.com>
2023-11-30 16:43:54 +0900

    MonsterMove
    MonsterMove

---
+++ Assets/Script/Monster/MonsterMove.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class MonsterMove : MonsterInfo
+{
+    [SerializeField] private float speed;
+    public Rigidbody2D targetPlayer;
+
+    SpriteRenderer sprite;
+    Rigidbody2D monsterRigidbody;
+    
+
+    private void Awake()
+    {
+        monsterRigidbody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    { 
+        // 몬스터의 방향
+        Vector2 direction = targetPlayer.position - monsterRigidbody.position;
+        monsterRigidbody.MovePosition(monsterRigidbody.position + (direction.normalized * speed * Time.fixedDeltaTime));
+
+        monsterRigidbody.velocity = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        // 플레이어 방향으로 쳐다보기
+        // sprite.flipX = targetPlayer.position.x < 0;
+        sprite.flipX = targetPlayer.position.x < monsterRigidbody.position.x;
+    }
+}
