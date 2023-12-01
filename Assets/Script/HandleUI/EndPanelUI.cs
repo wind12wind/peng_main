c14a02348db824b2be8c9cfd9bc68dda483f49bb
jeire07 <149458625+jeire07@users.noreply.github.com>
2023-12-01 14:08:12 +0900

    modify EndPanel
    EndPanel has been changed to a Prefab

---
+++ Assets/Script/HandleUI/EndPanelUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndPanelUI : MonoBehaviour
+{
+    public TextMeshProUGUI _bestTimeText { get; set; }
+    public TextMeshProUGUI _currentTimeText { get; set; }
+    public TextMeshProUGUI _bestKillText { get; set; }
+    public TextMeshProUGUI _currentKillText { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
