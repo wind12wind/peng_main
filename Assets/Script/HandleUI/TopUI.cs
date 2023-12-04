3208f707017655b7a3e93e553aa89f020277e4d2
jeire07 <149458625+jeire07@users.noreply.github.com>
2023-12-04 15:10:43 +0900

    Update TopUI.cs
    Decouple TopUI class from the parent class

--- Assets/Script/HandleUI/TopUI.cs
+++ Assets/Script/HandleUI/TopUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class TopUI : LevelManager
+public class TopUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _currentTimeText;
 
