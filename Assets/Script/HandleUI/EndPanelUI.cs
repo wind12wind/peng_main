2ba4f91fadbedc50047ab201405abdd244cd05e2
jeire07 <149458625+jeire07@users.noreply.github.com>
2023-12-06 19:08:13 +0900

    Fix UI
    UI did not matched on Screen, so fix UI scale for screen size automatically

--- Assets/Script/HandleUI/EndPanelUI.cs
+++ Assets/Script/HandleUI/EndPanelUI.cs
@@ -3,12 +3,27 @@ using TMPro;
 
 public class EndPanelUI : MonoBehaviour
 {
+    public static EndPanelUI Instance { get; private set; }
+
     [SerializeField] private GameObject _EndPanel;
 
     [SerializeField] private TextMeshProUGUI _bestTimeText;
     [SerializeField] private TextMeshProUGUI _currentTimeText;
     [SerializeField] private TextMeshProUGUI _bestKillText;
     [SerializeField] private TextMeshProUGUI _currentKillText;
+
+    private void Awake()
+    {
+        //Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
